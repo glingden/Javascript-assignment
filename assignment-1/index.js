2588512f7a2bccb4bbc04316d7a738b30b44a8e8
Ganga Lingden <lingden.ganga@gmail.com>
2022-04-13 11:28:59 +0300

    first assignment

--- assignment-1/index.js
+++ assignment-1/index.js
@@ -1,1 +1,27 @@
 // Your code here
+
+
+// Function to create a promise
+function createPromise() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve('resolved');
+    }, 100);
+  });
+}
+
+
+// Create asynchromous action
+async function prinNumber() {
+  let i = 1;
+  console.log('Number printing .....');
+  while(i <= 1000000){
+    const result = await createPromise(); // pause
+    console.log(i); // print number
+    i++;
+
+  }
+  
+}
+
+prinNumber();
